d2b534dacbf13110827dc7b9d65e2be1224e8bc5
Victoria Kravchenko <victoria.kr.1990@gmail.com>
2016-01-20 00:50:14 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require("gulp"),
+    concatCss = require('gulp-concat-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    browserSync = require('browser-sync'),
+    minifyCss = require('gulp-minify-css'),
+    mainBowerFiles = require('main-bower-files');
+
+gulp.task('css-bower', function() {
+    return gulp.src(mainBowerFiles('**/*.css'))        
+        .pipe(gulp.dest('app/source/css/lib'));
+});
+
+gulp.task('css', ['css-bower'], function () {
+  return gulp.src('app/source/css/**/*.css')
+    .pipe(concatCss('bundle.min.css'))
+    .pipe(minifyCss())
+    .pipe(gulp.dest('app/build/css'));
+});
+
+gulp.task('html', function () {
+  return gulp.src('app/source/*.html')
+    .pipe(gulp.dest('app/build/'));
+});
+
+gulp.task('js-bower', function() {
+    return gulp.src(mainBowerFiles('**/*.js'))        
+        .pipe(gulp.dest('app/source/js/lib'));
+});
+
+gulp.task('js', ['js-bower'], function() {
+    return gulp.src('app/source/js/**/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/build/js'));
+});
+
+// Сервер
+gulp.task('server', function () {  
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'app/build'
+    }
+  });
+});
+
+// Слежка
+gulp.task('watch', function () {
+  gulp.watch([
+    'app/source/*.html',
+    'app/source/js/**/*.js',
+    'app/source/css/**/*.css'
+  ], ['compile', browserSync.reload]);
+});
+
+gulp.task('compile', ['html', 'css', 'js']);
+
+// Задача по-умолчанию
+gulp.task('default', ['compile', 'server', 'watch']);
+
