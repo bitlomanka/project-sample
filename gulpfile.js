74dad4793fa1dfe3ac0be44f521265313c69d07b
Victoria Kravchenko <victoria.kr.1990@gmail.com>
2016-01-23 23:45:36 +0200

    added net tasks

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,18 @@ gulp.task('html', function () {
     .pipe(gulp.dest('app/build/'));
 });
 
+gulp.task('images', function () {
+  return gulp.src('app/source/images/**/*.*')
+    .pipe(gulp.dest('app/build/images'));
+});
+
+// font
+gulp.task('fonts', function () {
+  return gulp.src('app/source/fonts/**/*.*')
+    .pipe(gulp.dest('app/build/fonts'));
+});
+
+
 gulp.task('js-bower', function() {
     return gulp.src(mainBowerFiles('**/*.js'))        
         .pipe(gulp.dest('app/source/js/lib'));
@@ -50,11 +62,12 @@ gulp.task('watch', function () {
   gulp.watch([
     'app/source/*.html',
     'app/source/js/**/*.js',
-    'app/source/css/**/*.css'
+    'app/source/css/**/*.css',
+    'app/source/images/**/*.*',
   ], ['compile', browserSync.reload]);
 });
 
-gulp.task('compile', ['html', 'css', 'js']);
+gulp.task('compile', ['html', 'fonts', 'css', 'images', 'js']);
 
 // Задача по-умолчанию
 gulp.task('default', ['compile', 'server', 'watch']);
