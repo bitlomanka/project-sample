f265273543795f92011244531872320d8b53e718
Виктория Кравченко <vica@MacBook-Pro-Sergey.local>
2016-07-07 21:14:32 +0300

    updated sample

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,76 @@
 var gulp = require("gulp"),
-    concatCss = require('gulp-concat-css'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),    
     browserSync = require('browser-sync'),
-    minifyCss = require('gulp-minify-css'),
-    mainBowerFiles = require('main-bower-files');
+    postcss = require('gulp-postcss'),    
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    jade = require('gulp-jade'),
+    plumber = require('gulp-plumber'),
+    compass = require('gulp-compass');
 
-gulp.task('css-bower', function() {
-    return gulp.src(mainBowerFiles('**/*.css'))        
-        .pipe(gulp.dest('app/source/css/lib'));
+// jade
+gulp.task('jade', function() {
+  var YOUR_LOCALS = {};
+ 
+  return gulp.src('app/source/jade/pages/*.jade')
+    .pipe(plumber())
+    .pipe(jade({
+      locals: YOUR_LOCALS,
+      pretty: '\t'
+    }))
+    .pipe(gulp.dest('app/source/'))
 });
 
-gulp.task('css', ['css-bower'], function () {
-  return gulp.src('app/source/css/**/*.css')
-    .pipe(concatCss('bundle.min.css'))
-    .pipe(minifyCss())
-    .pipe(gulp.dest('app/build/css'));
+gulp.task('compass', function() {
+  return gulp.src('app/source/sass/**/*.scss')
+    .pipe(plumber())
+    .pipe(compass({
+      config_file: './config.rb',
+      css: 'app/source/css',
+      sass: 'app/source/sass',
+      image: 'app/source/images'
+    }))
 });
 
-gulp.task('html', function () {
-  return gulp.src('app/source/*.html')
-    .pipe(gulp.dest('app/build/'));
+
+// css/js/html
+gulp.task('html', ['jade', 'compass'], function () {
+    return gulp.src('app/source/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', postcss([
+                autoprefixer({ browsers: ['last 2 versions'] }),
+                cssnano
+            ])
+         ))        
+        .pipe(gulp.dest('app/build'));
 });
 
+// картинки
 gulp.task('images', function () {
   return gulp.src('app/source/images/**/*.*')
     .pipe(gulp.dest('app/build/images'));
 });
 
-// font
+// шрифты
 gulp.task('fonts', function () {
   return gulp.src('app/source/fonts/**/*.*')
-    .pipe(gulp.dest('app/build/fonts'));
+    .pipe(gulp.dest('app/build/css'));
+});
+    
+// сервер dev
+gulp.task('devServer', function () {  
+  browserSync({
+    port: 9000,
+    server: {
+      baseDir: 'app/source'
+    }
+  });
 });
 
-
-gulp.task('js-bower', function() {
-    return gulp.src(mainBowerFiles('**/*.js'))        
-        .pipe(gulp.dest('app/source/js/lib'));
-});
-
-gulp.task('js', ['js-bower'], function() {
-    return gulp.src('app/source/js/**/*.js')
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/build/js'));
-});
-
-// Сервер
+// сервер build
 gulp.task('server', function () {  
   browserSync({
     port: 9000,
@@ -57,18 +80,18 @@ gulp.task('server', function () {
   });
 });
 
-// Слежка
-gulp.task('watch', function () {
-  gulp.watch([
-    'app/source/*.html',
-    'app/source/js/**/*.js',
-    'app/source/css/**/*.css',
-    'app/source/images/**/*.*',
-  ], ['compile', browserSync.reload]);
+// слежка
+gulp.task('watch', function () {    
+  gulp.watch(['app/source/jade/**/*.jade'], ['jade', browserSync.reload]);
+  gulp.watch(['app/source/sass/**/*.scss'], ['compass', browserSync.reload]);
+  gulp.watch(['app/source/js/**/*.js'], [browserSync.reload]);
+}); 
+
+
+// сборка
+gulp.task('build', ['html', 'images', 'fonts'], function(){
+    gulp.start('server');
 });
 
-gulp.task('compile', ['html', 'fonts', 'css', 'images', 'js']);
-
-// Задача по-умолчанию
-gulp.task('default', ['compile', 'server', 'watch']);
-
+// таск по умолчанию
+gulp.task('default', ['jade', 'compass', 'devServer', 'watch']);
